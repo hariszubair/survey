bacfeb737d36ede627172da7876d1ee34ef1bcff
harisMac <haris.zubair@outlook.com>
2022-04-21 21:56:22 +0500

    first instance

---
+++ vue/src/store/index.js
@@ -0,0 +1,16 @@
+
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        user: {
+            name: 'haris'
+        }
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+    modules: {}
+})
+
+export default store;
